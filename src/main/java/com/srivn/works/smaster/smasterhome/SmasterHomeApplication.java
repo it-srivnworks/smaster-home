f7982c377fffb23e6a4017e7229929aa8f93f95d
dev02-srivnworks <dev02.srivnworks@gmail.com>
2022-10-15 01:04:01 +0100

    Added logback with profile dev/test

--- src/main/java/com/srivn/works/smaster/smasterhome/SmasterHomeApplication.java
+++ src/main/java/com/srivn/works/smaster/smasterhome/SmasterHomeApplication.java
@@ -2,12 +2,17 @@ package com.srivn.works.smaster.smasterhome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class SmasterHomeApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SmasterHomeApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SmasterHomeApplication.class, args);
+		logger.info("Starting SmasterHomeApplication");
 	}
 
 }
